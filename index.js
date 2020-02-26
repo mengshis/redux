d94682c86fa4fa541fe12acee2133b8a24ab0009
v_shimeng <v_shimeng@tencent.com>
2020-02-26 18:40:56 +0800

    Revert "test1"
    This reverts commit e4e77c7fe1678409c9bd300644288f5ac6787851.

--- index.js
+++ index.js
@@ -6,7 +6,6 @@
 const Store=require('./Store-3');
 exports.createStore=function createStore(updaters,defaultState) {
     console.log('test')
-    console.log('test')
     const sto=new Store(defaultState);
     sto.setUpdaters(updaters);
     return sto;
